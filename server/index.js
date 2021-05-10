6c11d89bff1273672cb92e448c4cd69d4941eda0
mitchelltjiii <mitchelltjiii@gmail.com>
2021-05-10 15:14:28 -0700

    initial commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 const path = require("path");
 const express = require("express");
 const app = express(); // create express app
+const port = process.env.PORT || 3000
+
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
@@ -11,7 +13,7 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-// start express server on port 5000
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+// start express server on port
+app.listen(port, () => {
+  console.log("server started on port " + port);
 });
