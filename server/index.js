c228f2f249c007f545f700e92242dc833fc0596e
mitchelltjiii <mitchelltjiii@gmail.com>
2021-05-11 06:26:43 -0700

    com

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const app = express(); // create express app
 const port = process.env.PORT || 3000
+const mysql = require('mysql');                                      
 
 
 // add middlewares
@@ -16,4 +17,19 @@ app.use((req, res, next) => {
 // start express server on port
 app.listen(port, () => {
   console.log("server started on port " + port);
+});
+
+const connection = mysql.createConnection({
+  host     : 'db-mysql-sfo3-15933-do-user-9039451-0.b.db.ondigitalocean.com',
+  user     : 'doadmin',
+  password : 'xo6wgtevue3qzrmw',
+  database : 'defaultdb'
+});
+
+connection.connect((err) => {
+  if (err) {
+      console.log('Connection error message: ' + err.message);
+      return;
+  }
+  console.log('Connected!')
 });
