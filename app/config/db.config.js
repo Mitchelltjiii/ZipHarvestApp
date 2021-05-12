8faf1c18aa85b5880595455dc8268cc17d217af9
mitchelltjiii <mitchelltjiii@gmail.com>
2021-05-12 15:26:41 -0700

    com

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,13 +3,12 @@
 const env = require('./production-env.js');
 
 const Sequelize = require('sequelize');
-const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize(env.database, env.db.username, env.db.password, {
   host: env.db.host,
   port: env.db.port,
   dialect: env.dialect,
-  dialectModule: mysql2,
+  dialectModule: "mysql2",
   operatorsAliases: false,
  
   pool: {
