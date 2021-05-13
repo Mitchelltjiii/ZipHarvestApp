92e09e94e7ab84a834c422cf8bed494311bae8d0
mitchelltjiii <mitchelltjiii@gmail.com>
2021-05-13 11:51:54 -0700

    com

--- app/routers/router.js
+++ app/routers/router.js
@@ -4,15 +4,13 @@ let router = express.Router();
  
 const controller = require('../controllers/controller.js');
 
-router.post('/api/plant', function(req, res){
-    controller.createPlant
-  });
+router.post('/api/plant', controller.createPlant);
 router.get('/api/plant/:id', controller.getPlant);
 router.get('/api/plants', controller.plants);
-router.put('/api/plant', function(req, res){
-    controller.updatePlant
-  });
+router.put('/api/plant', controller.updatePlant);
 router.delete('/api/plant/:id', controller.deletePlant);
+
+/*
 
 router.post('/api/harvestedplant', controller.createHarvestedPlant);
 router.get('/api/harvestedplant/:id', controller.getHarvestedPlant);
@@ -25,5 +23,5 @@ router.get('/api/harvestbatch/:id', controller.getHarvestBatch);
 router.get('/api/harvestbatches', controller.harvestBatches);
 router.put('/api/harvestbatch', controller.updateHarvestBatch);
 router.delete('/api/harvestbatch/:id', controller.deleteHarvestBatch);
-
+*/
 module.exports = router;
