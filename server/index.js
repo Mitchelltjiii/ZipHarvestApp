9c0ab645555621150f5dd3964f5006263c4fa5f3
mitchelltjiii <mitchelltjiii@gmail.com>
2021-05-13 14:35:54 -0700

    com

--- server/index.js
+++ server/index.js
@@ -24,11 +24,12 @@ let router = require('../app/routers/router');
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
+app.use('/', router);
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-app.use('/', router);
 
 
 /*
