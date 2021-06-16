53ad5adfaedd67bd89a2fa4bee4bc738401f4154
mitchelltjiii <mitchelltjiii@gmail.com>
2021-06-15 18:34:18 -0700

    com

--- src/components/ExportButton.component.js
+++ src/components/ExportButton.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import { CSVLink } from "react-csv";
 
 class ExportButton extends Component{
 
@@ -9,6 +10,18 @@ class ExportButton extends Component{
 
       //this.props.hideHarvestRecord(this.props.row.tag);
     }
+
+    headers = [
+        { label: "First Name", key: "firstname" },
+        { label: "Last Name", key: "lastname" },
+        { label: "Email", key: "email" }
+      ];
+       
+      data = [
+        { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+        { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+        { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" }
+      ];
     
     constructor(props) {
         super(props);
@@ -27,10 +40,13 @@ class ExportButton extends Component{
 
     render() {  
         return <div style={{width: "170px"}}>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleSubmit}  style={{width: "120px"}}>Export</Button>
+            <CSVLink data={data} headers={headers}>
+                Download me
+            </CSVLink>;
         </div>;
       }
 }
+//            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleSubmit}  style={{width: "120px"}}>Export</Button>
 
 
 
