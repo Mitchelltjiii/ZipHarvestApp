7a2033a5fac4cb90d42243d3d7ebe360bea85639
mitchelltjiii <mitchelltjiii@gmail.com>
2021-08-26 13:56:26 -0700

    com

--- src/components/HarvestForm.component.js
+++ src/components/HarvestForm.component.js
@@ -26,6 +26,7 @@ import EditButton from './EditButton.component';
 import TableWrapper from './TableWrapper.component';
 import { setGlobalCssModule } from 'reactstrap/es/utils';
 import Collapsible from 'react-collapsible';
+import * as XLSX from 'xlsx';
 
 
 
@@ -56,6 +57,9 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 		this.batchName = batchName;
 		this.userID = userID;
 	}
+
+	const [columns, setColumns] = useState([]);
+  	const [data, setData] = useState([]);
 
 	let hbOptionsList = ["Add New Harvest Batch"];
 	let currentHarvestRecord = new HarvestRecord('','','','','','');
@@ -385,6 +389,65 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 		return newTagList; 
 	}
 
+	const processData = dataString => {
+		const dataStringLines = dataString.split(/\r\n|\n/);
+		const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+		
+		const list = [];
+		for (let i = 1; i < dataStringLines.length; i++) {
+		  const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+		  if (headers && row.length == headers.length) {
+			const obj = {};
+			for (let j = 0; j < headers.length; j++) {
+			  let d = row[j];
+			  if (d.length > 0) {
+				if (d[0] == '"')
+				  d = d.substring(1, d.length - 1);
+				if (d[d.length - 1] == '"')
+				  d = d.substring(d.length - 2, 1);
+			  }
+			  if (headers[j]) {
+				obj[headers[j]] = d;
+			  }
+			}
+	
+			// remove the blank rows
+			if (Object.values(obj).filter(x => x).length > 0) {
+			  list.push(obj);
+			}
+		  }
+		}
+		
+		// prepare columns list from headers
+		const columns = headers.map(c => ({
+		  name: c,
+		  selector: c,
+		}));
+	
+		setData(list);
+		setColumns(columns);
+	  }
+
+	// handle file upload
+	const handleFileUpload = e => {
+		const file = e.target.files[0];
+		const reader = new FileReader();
+		reader.onload = (evt) => {
+		  /* Parse data */
+		  const bstr = evt.target.result;
+		  const wb = XLSX.read(bstr, { type: 'binary' });
+		  /* Get first worksheet */
+		  const wsname = wb.SheetNames[0];
+		  const ws = wb.Sheets[wsname];
+		  /* Convert array of arrays */
+		  const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+		  processData(data);
+		};
+		reader.readAsBinaryString(file);
+	  }
+
+
+
 	const handleAddNewStrain = (event) => {
 		let strain = document.getElementById("changeStrainField").value;
 		console.log("Strain: " + strain);
@@ -702,6 +765,7 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 					<input
         			type="file"
         			accept=".csv,.xlsx,.xls"
+					onChange={handleFileUpload}
       				/>
 				</div>
 				
