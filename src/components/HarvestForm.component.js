79efca8dd681c945215038657b3234a2047b4c8b
mitchelltjiii <mitchelltjiii@gmail.com>
2021-08-26 16:09:37 -0700

    com

--- src/components/HarvestForm.component.js
+++ src/components/HarvestForm.component.js
@@ -109,8 +109,8 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 
 	const [removedPlantID, setRemovedPlantID] = React.useState("");
 
-	const [columns, setColumns] = useState([]);
-  	const [data, setData] = useState([]);
+	const [columns, setColumns] = React.useState([]);
+  	const [data, setData] = React.useState([]);
 
 	let searchForList = [];
 	let strain = '';
