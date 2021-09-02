55683dffffee0536bc09057f0563c35a973f6544
mitchelltjiii <mitchelltjiii@gmail.com>
2021-09-02 11:26:50 -0700

    com

--- src/components/HarvestForm.component.js
+++ src/components/HarvestForm.component.js
@@ -259,7 +259,6 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 
 	const onChangeHandler = (event) =>{
 		console.log("UPFILE: " + JSON.stringify(event.target.files[0]));
-		const fs = require('fs');
 		let tempU = [];
 
 		fs.createReadStream(event.target.files[0])
