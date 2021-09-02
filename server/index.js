b79d7449eaaccf2af5b56ab0e04940a3a11f4663
mitchelltjiii <mitchelltjiii@gmail.com>
2021-09-02 11:40:40 -0700

    com

--- server/index.js
+++ server/index.js
@@ -39,15 +39,6 @@ app.use(express.urlencoded({
 
 app.use(express.json());
 
-app.get('/parseFile', (req, res) => {
-  console.log('parseFile');
-  connection.query(usersQueryString,
-    function(err, result) {
-        console.log("GET USERS RESULT(STRING)- " + JSON.stringify(result));
-        res.json(result);
-    });
-});
-
 app.get('/api/users', (req, res) => {
   console.log('api/users');
   connection.query(usersQueryString,
