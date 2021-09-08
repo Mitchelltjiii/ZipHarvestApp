44126a0a214430610fef1f5b42f997251a4e1c91
mitchelltjiii <mitchelltjiii@gmail.com>
2021-09-07 18:06:57 -0700

    com

--- src/components/HarvestForm.component.js
+++ src/components/HarvestForm.component.js
@@ -27,7 +27,6 @@ import TableWrapper from './TableWrapper.component';
 import { setGlobalCssModule } from 'reactstrap/es/utils';
 import Collapsible from 'react-collapsible';
 import CSVReader1 from './CsvReader1';
-import RemoveUploadQueueItemButton from './RemoveUploadQueueItemButton';
 
 
 
@@ -109,18 +108,6 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 	const [editMode, setEditMode] = React.useState(false);
 
 	const [removedPlantID, setRemovedPlantID] = React.useState("");
-
-	const [uploadList,setUploadList] = React.useState([]);
-
-	console.log("Upload List after refresh: " + JSON.stringify(uploadList));
-
-	let uploadNamesList = [];
-	for(const val of uploadList){
-		console.log('UploadList[m]: ' + val);
-		let splitList = val.split(",");
-		console.log("SplitList[0]: " + splitList[0]);
-		uploadNamesList.push(splitList[0]);
-	}
 
 	let searchForList = [];
 	let strain = '';
@@ -655,30 +642,6 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 		}
 	}
 
-	function removeUploadQueueButton(name){
-		console.log("removeUploadQueueButton--");
-		let index = 0;
-		let foundIndex = -1;
-		for(let val of uploadList){
-			let splitList = val.split(",");
-			console.log("SplitList[0]: " + splitList[0]);
-			if(splitList[0]==name){
-				foundIndex = index;
-				console.log("Found Index: " + foundIndex);
-			}
-			index++;
-		}
-
-		if(foundIndex != -1){
-			console.log("Found Index != -1");
-			let uList = uploadList;
-			uList.splice(foundIndex,1);
-			console.log("Upload List: " + JSON.stringify(uList));
-			setUploadList(uList);
-			refreshOuter();	
-		}
-	}
-
 	const ChangeHBForm = ({changeHBHiddenNow}) => {
 		if (changeHBHiddenNow) return null;
 	  
@@ -721,53 +684,10 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 		);
 	  };
 
-	  const setPlantList = (fn,pl) => {
-		let uList = uploadList;
-		uList.push(fn + "," + pl);
-		console.log("Upload List: " + JSON.stringify(uList));
-		setUploadList(uList);
-		refreshOuter();
-	  }
+	  
 
-	  const UploadTab = ({name}) => {
-		  return (
-			<div className="full tr">
-				<Grid
-					container
-					direction="row"
-  					justify="center"
-					alignItems="center"
-				>
-				<div>
-					{name}
-				</div>
-				<RemoveUploadQueueItemButton name={name} removeUploadQueueButton={removeUploadQueueButton}></RemoveUploadQueueItemButton>
-				</Grid>
-			</div>
-		  );
-	  };
+	  
 
-	  const ImportTab = () => {
-		return (
-		  <div className="full tr">
-			  <Grid
-					container
-					direction="column"
-  					justify="center"
-					alignItems="center"
-				>
-				<div>
-					<CSVReader1 setPlantList={setPlantList}></CSVReader1>
-				</div>
-				<div>
-					{uploadNamesList.map((name,index) => (
-            			<UploadTab name={name}></UploadTab>
-          			))}
-				</div>
-				</Grid>		  
-		  </div>
-		);
-	  };
 //<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleImport}>Import</Button>
 
 
@@ -1002,17 +922,6 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
   				justify="center"
 				alignItems="center"
 			>
-
-				<Grid
-					container
-					direction="row"
-  					justify="center"
-					alignItems="center"
-				>
-
-				<ImportTab></ImportTab>
-
-				</Grid>
 				<Grid
 					container
 					direction="row"
--- src/components/ManagePlantsForm.component.js
+++ src/components/ManagePlantsForm.component.js
@@ -2,8 +2,71 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import HBTable from './HBTable.component';
+import RemoveUploadQueueItemButton from './RemoveUploadQueueItemButton';
 
 function ManagePlantsForm({getHarvestBatches, getHarvestRecords, getPlants}) {
+
+    const [uploadList,setUploadList] = React.useState([]);
+
+    console.log("Upload List after refresh: " + JSON.stringify(uploadList));
+
+	let uploadNamesList = [];
+	for(const val of uploadList){
+		console.log('UploadList[m]: ' + val);
+		let splitList = val.split(",");
+		console.log("SplitList[0]: " + splitList[0]);
+		uploadNamesList.push(splitList[0]);
+	}
+
+    const setPlantList = (fn,pl) => {
+		let uList = uploadList;
+		uList.push(fn + "," + pl);
+		console.log("Upload List: " + JSON.stringify(uList));
+		setUploadList(uList);
+		refreshOuter();
+	  }
+
+    function removeUploadQueueButton(name){
+		console.log("removeUploadQueueButton--");
+		let index = 0;
+		let foundIndex = -1;
+		for(let val of uploadList){
+			let splitList = val.split(",");
+			console.log("SplitList[0]: " + splitList[0]);
+			if(splitList[0]==name){
+				foundIndex = index;
+				console.log("Found Index: " + foundIndex);
+			}
+			index++;
+		}
+
+		if(foundIndex != -1){
+			console.log("Found Index != -1");
+			let uList = uploadList;
+			uList.splice(foundIndex,1);
+			console.log("Upload List: " + JSON.stringify(uList));
+			setUploadList(uList);
+			refreshOuter();	
+		}
+	}
+
+    const UploadTab = ({name}) => {
+        return (
+          <div className="full tr">
+              <Grid
+                  container
+                  direction="row"
+                    justify="center"
+                  alignItems="center"
+              >
+              <div>
+                  {name}
+              </div>
+              <RemoveUploadQueueItemButton name={name} removeUploadQueueButton={removeUploadQueueButton}></RemoveUploadQueueItemButton>
+              </Grid>
+          </div>
+        );
+    };
 
 	return (
 		<div id="harvest-batches-form">
@@ -13,7 +76,23 @@ function ManagePlantsForm({getHarvestBatches, getHarvestRecords, getPlants}) {
   				justify="center"
 				alignItems="center"
 			>
-                <div>ManagePlants</div>
+                <div className="full tr">
+			    <Grid
+					container
+					direction="column"
+  					justify="center"
+					alignItems="center"
+				>
+				<div>
+					<CSVReader1 setPlantList={setPlantList}></CSVReader1>
+				</div>
+				<div>
+					{uploadNamesList.map((name,index) => (
+            			<UploadTab name={name}></UploadTab>
+          			))}
+				</div>
+				</Grid>		  
+		  </div>
 			</Grid>
 		</div>
 	);
