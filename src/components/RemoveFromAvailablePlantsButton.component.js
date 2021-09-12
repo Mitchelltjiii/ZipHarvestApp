74abc99019611241ad44eb36ff324c56942504c0
mitchelltjiii <mitchelltjiii@gmail.com>
2021-09-11 21:25:19 -0700

    com

--- src/components/RemoveFromAvailablePlantsButton.component.js
+++ src/components/RemoveFromAvailablePlantsButton.component.js
@@ -14,7 +14,7 @@ class RemoveFromAvailablePlants extends Component{
         console.log("Engage add Plant");
         let parent = this;
         const resp = fetch('/pl', {
-          method: (plantItem.id) ? 'PUT' : 'POST',
+          method: (plantItem.tag) ? 'PUT' : 'POST',
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -100,7 +100,7 @@ class RemoveFromAvailablePlants extends Component{
 
 			        for(const val2 of JSON.parse(this.props.getPlants())){
                 console.log("Checking against plant: " + JSON.stringify(val2));
-                if(val2.tag == val){
+                //if(val2.tag == val){
                   addPlant = new Plant(val2.tag,val2.strain,this.props.userID,1);
 
                   const plantItem = getPlantItem(addPlant);
@@ -115,7 +115,7 @@ class RemoveFromAvailablePlants extends Component{
 
                   this.addPlant(plantItem);
                   console.log("After addPlant");
-                }
+                //}
               }
             }
 
