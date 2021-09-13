510655cce838c859f7a287addac582647ca7b597
mitchelltjiii <mitchelltjiii@gmail.com>
2021-09-13 13:39:50 -0700

    com

--- src/components/PlantTable.component.jsx
+++ src/components/PlantTable.component.jsx
@@ -42,7 +42,7 @@ function PlantTable({getPlants,toggleDeleteAllSelected,getDeleteAllSelected,togg
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-          <TableCell align="left" size="small">
+          <TableCell align="left">
             <div>
                 <SelectDeleteAllButton toggleDeleteAllSelected={toggleDeleteAllSelected} getDeleteAllSelected={getDeleteAllSelected}></SelectDeleteAllButton>
             </div>
@@ -54,7 +54,7 @@ function PlantTable({getPlants,toggleDeleteAllSelected,getDeleteAllSelected,togg
         <TableBody>
             {rows.map((row) => (
             <TableRow key={row.tag}>
-              <TableCell align="left" padding="checkbox">
+              <TableCell align="left">
                   <SelectDeletePlantButton toggleDeletePlantSelected={toggleDeletePlantSelected} getDeletePlantSelected={getDeletePlantSelected} tag={row.tag}></SelectDeletePlantButton>
               </TableCell>
               <TableCell component="th" scope="row">
