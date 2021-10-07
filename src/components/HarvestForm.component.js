d0ccb7470e04bc3c6a4f85033c5425a352f7b621
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-07 13:39:38 -0700

    com

--- src/components/HarvestForm.component.js
+++ src/components/HarvestForm.component.js
@@ -28,7 +28,7 @@ import { setGlobalCssModule } from 'reactstrap/es/utils';
 import Collapsible from 'react-collapsible';
 import Dictaphone from './Dictaphone.component';
 import LastHarvested from './LastHarvested.component';
-
+import edit from '../edit.png';
 
 
 function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,getHarvestRecords,setHarvestRecords,resetHarvestBatches, 
@@ -790,7 +790,9 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
 						<FormLabel id="date-label" style={{verticalAlign: "center",align:"center",marginTop:"2px",marginBottom:"2px",marginLeft:"2px",marginRight:"2px"}}><b>{harvestDateLabelText}</b></FormLabel>
 					</div>
 					<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleEditHarvestDate} 
-				style={{maxHeight:"30px",minHeight:"30px",maxWidth:"40px",minWidth:"40px",fontSize:"12px",marginTop:"2px",marginBottom:"2px",marginLeft:"2px",marginRight:"2px"}}>Edit</Button>
+				style={{maxHeight:"30px",minHeight:"30px",maxWidth:"40px",minWidth:"40px",fontSize:"12px",marginTop:"2px",marginBottom:"2px",marginLeft:"2px",marginRight:"2px"}}>
+					<img src={edit} style={{maxHeight: "60px"}}/>
+				</Button>
 				</Grid>
 			  		  
 		  </div>
--- src/components/VoiceButton.component.js
+++ src/components/VoiceButton.component.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Button from '@material-ui/core/Button';
 import mic from '../mic.png';
-import edit from '../edit.png';
 
 class VoiceButton extends Component{
     handleClick(event) {
@@ -28,7 +27,7 @@ class VoiceButton extends Component{
         return <div>
             
 			<Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}  style={{width: "100%"}}>
-                <img src={edit} style={{maxHeight: "60px"}}/>
+                <img src={mic} style={{maxHeight: "60px"}}/>
             </Button>
 		
         </div>
