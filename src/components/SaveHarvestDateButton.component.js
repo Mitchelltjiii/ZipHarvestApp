e365f4a9f1ff2a1db459f41eb38c4bc0fa815b41
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-07 18:22:08 -0700

    com

--- src/components/SaveHarvestDateButton.component.js
+++ src/components/SaveHarvestDateButton.component.js
@@ -24,6 +24,7 @@ class SaveHarvestDateButton extends Component{
       this.props.setCurrentHarvestDate();
       const harvestBatchItem = this.props.getHarvestBatchItem(false);
       console.log("Harvest Batch Item should be done");
+      
 
       const resp = fetch('/hb', {
           method: (harvestBatchItem.name) ? 'PUT' : 'POST',
