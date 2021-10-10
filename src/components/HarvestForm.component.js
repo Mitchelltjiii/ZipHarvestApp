4c4bcc0dbc244936af5c4b590a23191963a36aae
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-10 12:16:35 -0700

    com

--- src/components/HarvestForm.component.js
+++ src/components/HarvestForm.component.js
@@ -901,7 +901,7 @@ function HarvestForm({getHarvestBatches,setHarvestBatches,getPlants,setPlants,ge
   					justifyContent="right"
 					alignItems="right"
 					>
-					<div style={{height: "100%",fontSize:"14px",marginTop:"5px",width:"100px"}}><b>{currentHarvest.date}</b></div>
+					<FormLabel id="harvest-date-label" style={{verticalAlign:"center",align:"center",marginTop:"2px",marginBottom:"2px",marginLeft:"2px",marginRight:"2px"}}><b>{harvestDateLabelText}</b></FormLabel>
 					<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleEditHarvestDate}  style={{minWidth: "30px",maxWidth: "30px",minHeight: "30px",maxHeight: "30px"}}>
                 	<img src={edit} style={{minWidth: "20px",maxWidth: "20px",minHeight: "20px",maxHeight: "20px"}}/>
 					</Button> 
