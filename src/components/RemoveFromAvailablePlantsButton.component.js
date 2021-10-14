d482ea549e304777924abea0567b9fac2b31c8cb
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-13 19:04:24 -0700

    com

--- src/components/RemoveFromAvailablePlantsButton.component.js
+++ src/components/RemoveFromAvailablePlantsButton.component.js
@@ -154,7 +154,7 @@ class RemoveFromAvailablePlants extends Component{
 
     render() {    
         return <div>
-            <Button variant="contained" aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleSubmit}>Delete</Button>
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleSubmit}>Delete</Button>
         </div>
       }
 }
