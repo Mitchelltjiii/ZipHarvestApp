afe714d0c61144884cf971f0fddc943bec63a902
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-19 18:06:50 -0700

    com

--- src/components/LogIn.component.jsx
+++ src/components/LogIn.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import {isMobile} from 'react-device-detect';
 
 function LogIn({getUsers, executeLogIn, reloadUsers,getUsersLoading,setUsers,attemptLogin}){
     const [username, setUsername] = React.useState('');
@@ -23,13 +24,20 @@ function LogIn({getUsers, executeLogIn, reloadUsers,getUsersLoading,setUsers,att
 		setPassword(event.target.value);
 	  };
 
+    let formWidth = "350px";
+    let formHeight = "500px";
+
+    if(isMobile){
+      formWidth = "230px";
+      formHeight = "330px";
+    }
 
     return(
       <div style={{
         position: 'absolute', left: '50%', top: '50%',
         transform: 'translate(-50%, -50%)'
     }}>
-      <div style={{width:"350px",height:"500px",border:"1px solid #d7d7d7",borderRadius:5}}>
+      <div style={{width:formWidth,height:formHeight,border:"1px solid #d7d7d7",borderRadius:5}}>
 				<div style={{
 			position: 'absolute', left: '50%', top: '50%',
 			transform: 'translate(-50%, -50%)'}}>
