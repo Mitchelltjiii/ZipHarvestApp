b9013395f5dda296ed8de6883b7dd06879d537dc
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-21 00:40:01 -0700

    com

--- src/components/PaymentForm.component.js
+++ src/components/PaymentForm.component.js
@@ -1,15 +1,20 @@
 import { withRouter } from "react-router-dom";
 import React from "react";
-import {CardElement, useElements} from '@stripe/react-stripe-js';
+import {CardElement, useElements, useStripe} from '@stripe/react-stripe-js';
 
 const PaymentForm = () => {
     const elements = useElements();
+    const stripe = useStripe();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        if(!stripe || !elements){
+            return;
+        }
         const cardElement = elements.getElement(CardElement);
-        console.log(cardElement);
+        console.log('card',cardElement);
+        console.log('stripe',stripe);
     }
     return (
     <form onSubmit={handleSubmit}>
