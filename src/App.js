da1a3c9159bd0a944b9fa6ef28e9f73a10a86440
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-20 18:25:44 -0700

    com

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./styles.css";
 import Header from './components/Header.component';
 import Outer from './components/Outer.component';
 import LogIn from './components/LogIn.component';
+import { loadStripe } from "@stripe/stripe-js";
+
 
 export default class App extends React.Component {
   state = {
@@ -256,6 +258,8 @@ export default class App extends React.Component {
   }
 
   render() {
+    const stripePromise = loadStripe("pk_test_51JmpUwGBqcLC10HcR83rJs3pzuuVNBccQnf6InpAaLtuTdo6SWH9ITX1QZcCFze1n2St0yk3PEa8flb4QHvSgMR000sINbKwaM");
+
     if ((this.state.loggedIn !== '' && (this.state.plantsLoading || this.state.harvestRecordsLoading || this.state.harvestBatchesLoading))){
       return(<div>Loading...</div>);
     }
