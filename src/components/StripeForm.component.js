7492afa95d641a14470fda6d767f918744d7e3da
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-26 08:53:48 -0700

    com

--- src/components/StripeForm.component.js
+++ src/components/StripeForm.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import '../App.css';
 
 const ProductDisplay = () => (
@@ -104,4 +104,4 @@ const Logo = () => (
     </g>
   </svg>
 );
-
+*/
