36f51ad7159f0c6870f95aa2f1ebebfe4ad0b0a9
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-25 18:44:56 -0700

    com

--- src/components/LogIn.component.jsx
+++ src/components/LogIn.component.jsx
@@ -3,7 +3,6 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import {isMobile} from 'react-device-detect';
-import StripeContainer from './StripeContainer';
 import Stripe from 'stripe';
 import StripeForm from './StipeForm.component';
 
