be556f951ba476f2eed73478f26a58e63814d0da
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-25 19:19:08 -0700

    com

--- src/components/SelectDeletePlantButton.component.js
+++ src/components/SelectDeletePlantButton.component.js
@@ -22,7 +22,6 @@ class SelectDeletePlantButton extends Component{
 
     render() {  
         let checked = this.props.getDeletePlantSelected(this.props.tag);
-        console.log("Select Delete Plant Button Checked: " + JSON.stringify(checked));
         return <div>
             {checked ? <Button variant="outlined" aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}  style={{height: "20px", width: "20px", minWidth: "20px", backgroundColor: "#A9A9A9"}} ></Button>
             :<Button variant="outlined" aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick} style={{height: "20px", width: "20px", minWidth: "20px"}}></Button>
