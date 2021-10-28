f8572f1edf6a7e4ebde00e758041bacf07e344a3
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-28 10:48:11 -0700

    com

--- src/App.js
+++ src/App.js
@@ -320,7 +320,7 @@ export default class App extends React.Component {
       reloadPlantsAndHarvestRecords={this.reloadPlantsAndHarvestRecords} reloadHarvestBatches={this.reloadHarvestBatches} reloadHarvestRecords={this.reloadHarvestRecords}/>
     </div>;
     }else{
-		showForm = <div><LogIn getUsers={this.getUsers} executeLogIn={this.executeLogIn} reloadUsers={this.reloadUsers} getUsersLoading={this.getUsersLoading} setUsers={this.setUsers} attemptLogin={this.attemptLogin} setCurrentPage={this.setCurrentPage}></LogIn></div>;
+		showForm = <div><ProductForm></ProductForm></div>;
     }
     return (
       <div className="App" style={{margin:"auto"}}>
@@ -329,3 +329,4 @@ export default class App extends React.Component {
     );
   }
 }
+//<LogIn getUsers={this.getUsers} executeLogIn={this.executeLogIn} reloadUsers={this.reloadUsers} getUsersLoading={this.getUsersLoading} setUsers={this.setUsers} attemptLogin={this.attemptLogin} setCurrentPage={this.setCurrentPage}></LogIn>
