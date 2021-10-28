4babadbd2cadbe59a535c0168a35cfba2adf4f99
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-28 12:03:04 -0700

    com

--- server/sendgridconfig.js
+++ server/sendgridconfig.js
@@ -1,3 +1,3 @@
 module.exports={
-SENDGRID_API: ‘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’
+SENDGRID_API: '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'
 }
