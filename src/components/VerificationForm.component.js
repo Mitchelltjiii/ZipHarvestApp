55d4ba50e4204ae4b5ddeaf6e0fcf62825ded086
mitchelltjiii <mitchelltjiii@gmail.com>
2021-10-31 16:54:25 -0700

    com

--- src/components/VerificationForm.component.js
+++ src/components/VerificationForm.component.js
@@ -87,6 +87,17 @@ function VerificationForm({refreshOuter, userID,setCurrentPage,possibleUsername}
       }
 
 
+    function makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * 
+          charactersLength));
+       }
+       return result;
+    }
+
     async function sendVerificationEmail(possibleSub){
         console.log("Try to send ver email");
         let address = "Mitchelltjiii@gmail.com";
