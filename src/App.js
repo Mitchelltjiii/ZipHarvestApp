d86adddc57e477c6912d34062089f0e8a68c11b9
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-01 22:28:05 -0700

    com

--- src/App.js
+++ src/App.js
@@ -35,18 +35,8 @@ export default class App extends React.Component {
   getUsersFromDB = async (username,password) => {
     const response = await fetch(`/api/users/${username}/${password}`);
     const text = await response.text();
-    const responseTwo = await fetch(`/create-customer`);
-    const json = await responseTwo.json();
-    try{
-      console.log("Customer JSON: " + json);
-    }catch(err){
-
-    }
-    try{
-      console.log("Customer JSON(STRING): " + JSON.stringify(json));
-    }catch(err){
-      
-    }
+    /*const responseTwo = await fetch(`/create-customer`);
+    const json = await responseTwo.json();*/
 
     this.state.users = text;
     this.state.usersLoading = false;
