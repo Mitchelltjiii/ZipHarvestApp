fdd31c0ec07c5c98a9da4ba9b827232694823b1b
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-06 13:52:08 -0700

    com

--- src/App.js
+++ src/App.js
@@ -342,7 +342,7 @@ export default class App extends React.Component {
       }
     }
 
-    if (this.state.loggedIn !== '' || this.state.currentPage === ('create-user-form') || this.state.currentPage === ('stripe-form') || this.state.currentPage === ('verification-form') || this.state.currentPage === ('reset-password-form') || this.state.currentPage === ('find-user-form')) {
+    if (this.state.loggedIn !== '') {
 	  	showForm = <div style={{margin:"auto"}}>
 	    <Header setCurrentPage={this.setCurrentPage} currentPage={this.state.currentPage} executeLogout={this.executeLogout}/>
       <Outer currentPage={this.state.currentPage} setCurrentPage={this.setCurrentPage} getPlants={this.getPlants} getHarvestRecords={this.getHarvestRecords} getHarvestBatches={this.getHarvestBatches}
