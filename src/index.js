1433eea3e62fc29eaf5923fe38c9530e78caeb3e
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-06 18:01:26 -0700

    com

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import { Elements } from "@stripe/react-stripe-js";
     const stripePromise = loadStripe("pk_test_51JmpUwGBqcLC10HcR83rJs3pzuuVNBccQnf6InpAaLtuTdo6SWH9ITX1QZcCFze1n2St0yk3PEa8flb4QHvSgMR000sINbKwaM")
 
     ReactDOM.render(
-        <React.StrictMode style={{innerHeight:"100%",outerHeight:"100%",innerWidth:"100%",outerWidth:"100%"}}>
-           <div style={{backgroundColor:"#a83232",height:"100%",width:"100%"}}></div>
+        <React.StrictMode>
+           <div style={{backgroundColor:"#a83232",height:"100%",width:"100%"}}>
+               Fuck
+           </div>
         </React.StrictMode>,
         document.getElementById("root")
     );
