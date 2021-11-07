4a2995ea1130caf2ac9d5f16e84e1e22ad5337f5
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-06 18:22:43 -0700

    com

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ import { Elements } from "@stripe/react-stripe-js";
     const stripePromise = loadStripe("pk_test_51JmpUwGBqcLC10HcR83rJs3pzuuVNBccQnf6InpAaLtuTdo6SWH9ITX1QZcCFze1n2St0yk3PEa8flb4QHvSgMR000sINbKwaM")
 
     ReactDOM.render(
+        <div>
         <Elements stripe={stripePromise}>
                 <App/>
-            </Elements>,
+            </Elements>
+            </div>,
         document.getElementById("root")
     );
+
+    /*<React.StrictMode>
+        </React.StrictMode> */
