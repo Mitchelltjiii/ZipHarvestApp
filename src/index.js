dd4efbeb1b0ca5dfa10562330a4d5651512d8dc1
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-07 10:38:39 -0800

    co

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Elements } from "@stripe/react-stripe-js";
 
     ReactDOM.render(
         <React.StrictMode>
-            <div style={{width:"100%",height:"100%",backgroundColor:"#a83232",padding:"0px"}}>
+            <div style={{width:"100%",height:"100%",backgroundColor:"#a83232",padding:"0px",margin:"0px"}}>
             </div>
         </React.StrictMode>,
         document.getElementById("root")
