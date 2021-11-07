cc1a5c18f14baf50da3d6734bd497c4c23135d4d
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-06 17:33:58 -0700

    com

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Elements } from "@stripe/react-stripe-js";
     const stripePromise = loadStripe("pk_test_51JmpUwGBqcLC10HcR83rJs3pzuuVNBccQnf6InpAaLtuTdo6SWH9ITX1QZcCFze1n2St0yk3PEa8flb4QHvSgMR000sINbKwaM")
 
     ReactDOM.render(
-        <React.StrictMode>
+        <React.StrictMode style={{innerHeight:"100%",outerHeight:"100%",innerWidth:"100%",outerWidth:"100%"}}>
             <Elements stripe={stripePromise} style={{innerHeight:"100%",outerHeight:"100%",innerWidth:"100%",outerWidth:"100%"}}>
-                <App/>
+                <App style={{innerHeight:"100%",outerHeight:"100%",innerWidth:"100%",outerWidth:"100%"}}/>
             </Elements>
         </React.StrictMode>,
         document.getElementById("root")
