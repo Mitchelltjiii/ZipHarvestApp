68fc7b8ce1ee3a46270c863ec3070aca4ea3a35d
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-08 20:40:58 -0800

    com

--- src/components/VerificationForm.component.js
+++ src/components/VerificationForm.component.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import {isMobile} from 'react-device-detect';
 
 function VerificationForm({setCurrentPage,newUsername}) {
 
@@ -140,18 +141,32 @@ function VerificationForm({setCurrentPage,newUsername}) {
 				alignItems="center"
 			>
                 <div style={{fontSize:"20px",marginTop:"10px",marginBottom:"10px"}}>Create your account</div>
-                <div style={{width:formWidth,height:formHeight,border:"1px solid #d7d7d7",borderRadius:5}}>
+                {isMobile ? 
+                                <div style={{width:formWidth,height:formHeight}}>
                 
-                        <Grid
-				container
-				direction="column"
-  				justifyContent="center"
-				alignItems="center"
-			    >
-                    <div>We sent you a link to verify your account. If you haven't recieved it, try checking your spam folder.</div>
-                    <div>If you are still having trouble, you can send a new code below. Code will expire 15 minutes after creation.</div>
-                    </Grid>
-                    </div>                
+                                <Grid
+                container
+                direction="column"
+                  justifyContent="center"
+                alignItems="center"
+                  >
+                            <div>We sent you a link to verify your account. If you haven't recieved it, try checking your spam folder.</div>
+                            <div>If you are still having trouble, you can send a new code below. Code will expire 15 minutes after creation.</div>
+                            </Grid>
+                            </div>
+                            :
+                            <div style={{width:formWidth,height:formHeight,border:"1px solid #d7d7d7",borderRadius:5}}>
+                
+                            <Grid
+            container
+            direction="column"
+              justifyContent="center"
+            alignItems="center"
+              >
+                        <div>We sent you a link to verify your account. If you haven't recieved it, try checking your spam folder.</div>
+                        <div>If you are still having trouble, you can send a new code below. Code will expire 15 minutes after creation.</div>
+                        </Grid>
+                        </div>}                
                 <Button style={{marginTop:"10px"}} variant="contained" aria-controls="simple-menu" aria-haspopup="true" onClick={handleResend}>Resend Code</Button>
 			</Grid>
 		</div>
