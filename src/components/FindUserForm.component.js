f8b22fb2abd6fdfff70832a6e0bc808fb8f72105
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-08 19:07:49 -0800

    vo,

--- src/components/FindUserForm.component.js
+++ src/components/FindUserForm.component.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import {isMobile} from 'react-device-detect';
 
 function FindUserForm({setCurrentPage}) {
 
