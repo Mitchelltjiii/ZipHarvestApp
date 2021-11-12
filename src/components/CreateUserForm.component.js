3febd1c3e044653c443740cb6d022edac5ca1c2e
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-11 16:07:43 -0800

    com

--- src/components/CreateUserForm.component.js
+++ src/components/CreateUserForm.component.js
@@ -3,7 +3,6 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import {isMobile} from 'react-device-detect';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import {InputAdornment,IconButton} from "@material-ui/core";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
