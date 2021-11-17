0c03a3135e88424605a62b3ad2c6960d435e4749
mitchelltjiii <mitchelltjiii@gmail.com>
2021-11-16 16:01:41 -0800

    com

--- src/components/AccountForm.component.js
+++ src/components/AccountForm.component.js
@@ -9,6 +9,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import {isMobile} from 'react-device-detect';
+
 
 function AccountForm({refreshOuter, userID, setCurrentPage, setFromAccountSettings}) {
 
