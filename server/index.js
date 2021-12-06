cd6736c57048fd84de031b3114cb050ab16a779d
mitchelltjiii <mitchelltjiii@gmail.com>
2021-12-06 11:46:02 -0800

    com

--- server/index.js
+++ server/index.js
@@ -145,28 +145,27 @@ app.get("/api/users/:username/:password",(req,res) => {
           let verified = false;
           let subscription = false;
 
-          const bcrypt = require('bcrypt');
           /*
+          const bcrypt = require('bcrypt');
           const saltRounds = 8;
-          const myPlaintextPassword = 'Mystr0ngP@SSW0RD';
+          const myPlaintextPassword = req.params.password;
           var hashAsync;
           var hashSync;
-          console.time('bcrypt_Async Generate Hash');
+          console.log('bcrypt_Async Generate Hash');
           bcrypt.hash(myPlaintextPassword, saltRounds).then(function (hash) {
             hashAsync = hash;
-            console.timeEnd('bcrypt_Async Generate Hash');
-            console.time('bcrypt_Async Compare Hash');
+            console.log('bcrypt_Async Generate Hash');
+            console.log('bcrypt_Async Compare Hash');
             bcrypt.compare(myPlaintextPassword, hashAsync).then(function (result) {
-              console.timeEnd('bcrypt_Async Compare Hash');
+              console.log('bcrypt_Async Compare Hash');
             });
           });
-          console.time('bcrypt_Sync Generate Hash');
+          console.log('bcrypt_Sync Generate Hash');
           hashSync = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-          console.timeEnd('bcrypt_Sync Generate Hash')
-          console.time('bcrypt_Sync Compare Hash');
+          console.log('bcrypt_Sync Generate Hash')
+          console.log('bcrypt_Sync Compare Hash');
           bcrypt.compareSync(myPlaintextPassword, hashSync);
-          console.timeEnd('bcrypt_Sync Compare Hash');
-          */
+          console.log('bcrypt_Sync Compare Hash');*/
 
           try{
             console.log("Trying iteration without parse");
