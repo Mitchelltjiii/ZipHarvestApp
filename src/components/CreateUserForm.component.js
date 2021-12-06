43f3fa00cf23b9ebf0513371896ffa65df17f2cc
mitchelltjiii <mitchelltjiii@gmail.com>
2021-12-06 11:02:40 -0800

    com

--- src/components/CreateUserForm.component.js
+++ src/components/CreateUserForm.component.js
@@ -266,17 +266,6 @@ function CreateUserForm({setCurrentPage,setNewUsername}) {
       }
     }
 
-    function makeid(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-          result += characters.charAt(Math.floor(Math.random() * 
-          charactersLength));
-       }
-       return result;
-    }
-
     function getUserItem(newCode){
       console.log("Enter getUserItem")
     
