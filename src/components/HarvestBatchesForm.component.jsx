385cf72c1f803f0700205c795ef4d43c559b30ee
mitchelltjiii <mitchelltjiii@gmail.com>
2021-12-12 15:54:33 -0800

    com

--- src/components/HarvestBatchesForm.component.jsx
+++ src/components/HarvestBatchesForm.component.jsx
@@ -34,8 +34,8 @@ function HarvestBatchesForm({getHarvestBatches, getHarvestRecords, getPlants, us
   					justifyContent="center"
 					alignItems="center"
 					>
-						<div style={{border:"medium solid #0000FF",fontSize:"20px",backgroundColor:"#59981A",widht:"100px",height:"50px",borderTopLeftRadius:"5px",borderTopLeftRadius:"5px"}}>{uniqueIDCount}/{possiblePlantCount}</div>
-						<div style={{border:"medium solid #0000FF",fontSize:"12px",widht:"100px",height:"50px",borderBottomLeftRadius:"5px",borderBottomRightRadius:"5px"}}>Unique tags exported this month</div>
+						<div style={{margin:"auto",border:"medium solid #000000",fontSize:"20px",backgroundColor:"#59981A",width:"100px",height:"50px",borderTopLeftRadius:"5px",borderTopRightRadius:"5px"}}>{uniqueIDCount}/{possiblePlantCount}</div>
+						<div style={{margin:"auto",border:"medium solid #000000",fontSize:"12px",width:"100px",height:"50px",borderBottomLeftRadius:"5px",borderBottomRightRadius:"5px"}}>Unique tags exported this month</div>
 					</Grid>
 					<div style={{margin:"auto"}}>
 					<HBTable getHarvestBatches={getHarvestBatches} getHarvestRecords={getHarvestRecords} getPlants={getPlants} userID={userID} reloadExportRecords={reloadExportRecords} getUniqueIDCount={getUniqueIDCount} getDryRooms={getDryRooms}></HBTable>
