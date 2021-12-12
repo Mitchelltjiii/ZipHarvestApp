39ffc799b772db20e9322f352a0abb02074de9e1
mitchelltjiii <mitchelltjiii@gmail.com>
2021-12-12 15:05:12 -0800

    com

--- src/components/HarvestBatchesForm.component.jsx
+++ src/components/HarvestBatchesForm.component.jsx
@@ -29,8 +29,8 @@ function HarvestBatchesForm({getHarvestBatches, getHarvestRecords, getPlants, us
   					justifyContent="center"
 					alignItems="center"
 					>
-						<div>{uniqueIDCount}/{possiblePlantCount}</div>
-						<div>Unique tags exported this month</div>
+						<div style={{fontSize:"16px",backgroundColor:"#59981A"}}>{uniqueIDCount}/{possiblePlantCount}</div>
+						<div style={{fontSize:"10px"}}>Unique tags exported this month</div>
 					</Grid>
 					<div style={{margin:"auto"}}>
 					<HBTable getHarvestBatches={getHarvestBatches} getHarvestRecords={getHarvestRecords} getPlants={getPlants} userID={userID} reloadExportRecords={reloadExportRecords} getUniqueIDCount={getUniqueIDCount} getDryRooms={getDryRooms}></HBTable>
