e77bd7ea87457845242557745debd53fb52c2e7e
mitchelltjiii <mitchelltjiii@gmail.com>
2021-12-31 08:56:18 -0800

    com

--- src/components/ExportButton.component.js
+++ src/components/ExportButton.component.js
@@ -104,14 +104,18 @@ class ExportButton extends Component{
 
         function commitExportRecords(){
           console.log("Commit Export Records");
-          
+
           console.log("Exports Record Data: " + JSON.stringify(exportRecordsData))
             for(const val of exportRecordsData){
                 busyCreatingExportRecords.push(val);
+                console.log("Busy Creating added: " + JSON.stringify(val))
+
             }
 
             for(const val of exportRecordsData){
                 createExportRecord(val);
+                console.log("Create export Record: " + JSON.stringify(val))
+
             }
 
             let timeLimit = 3000;
