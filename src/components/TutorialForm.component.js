bf2fd9c2e230bb7737d06079012cf4385985c4be
mitchelltjiii <mitchelltjiii@gmail.com>
2022-01-14 16:57:29 -0800

    com

--- src/components/TutorialForm.component.js
+++ src/components/TutorialForm.component.js
@@ -2,6 +2,13 @@ import React from 'react';
 import {isMobile} from 'react-device-detect';
 import Grid from '@material-ui/core/Grid';
 
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 function TutorialForm() {
 
     let formWidth = "800px";
