407457d792c0433c6b6e7557f752c5d892efa69e
mitchelltjiii <mitchelltjiii@gmail.com>
2022-01-15 14:45:35 -0800

    com

--- src/components/StripeForm.js
+++ src/components/StripeForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import {isMobile} from 'react-device-detect';
-import pricingImage from '../pricing.png';
+import pricingImage from '../pricing.svg';
 
 export default function StripeForm({verCode,userFromUrl,userFromLogin}) {
 
