18eb166418fd1e3a4280f89531bd27b50a521cd9
mitchelltjiii <mitchelltjiii@gmail.com>
2022-03-06 18:31:42 -0800

    com

--- src/components/TutorialForm.component.js
+++ src/components/TutorialForm.component.js
@@ -89,7 +89,7 @@ Click “Next Plant”.</div>
     return(
       <div id="tutorial-form" style={{position:"absolute",top:"50px",bottom:"0px",left:"0px",right:"0px",display:'flex',alignItems: 'center',justifyContent: 'center'}}>     
       <ReactPlayer
-        url="https://vimeo.com/681638851"
+        url="https://vimeo.com/685299729"
       />
 		</div>
     );
