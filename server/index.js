f19cb38c3580c51b12b7a8303cd8f1ccd4ff6d9e
mitchelltjiii <mitchelltjiii@gmail.com>
2022-04-22 18:21:45 -0700

    com

--- server/index.js
+++ server/index.js
@@ -345,17 +345,17 @@ app.get('/update-subscription/:subid/:priceid', async (req,res) =>{
   let newSub = await stripe.subscriptions.update(req.params.subid, {
   cancel_at_period_end: false,
   proration_behavior: 'create_prorations',
-  items: {
+  items: [{
     data: [{
       id: subscription.items.data[0].id,
-      price: {
+      price: [{
           id: req.params.priceid,
           lookup_key: price.lookup_key,
           unit_amount: unitAmount,
           unit_amount_decimal: unitAmountDecimal
-      },
+      }],
     }]
-  }
+  }]
 });
 console.log("New Sub: " + JSON.stringify(newSub));
   res.json(newSub);
