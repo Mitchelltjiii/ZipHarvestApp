b433dfee69ca64c2520d097e338c3f643f6e7020
mitchelltjiii <mitchelltjiii@gmail.com>
2022-04-24 11:40:43 -0700

    com

--- src/components/StripeForm.js
+++ src/components/StripeForm.js
@@ -30,7 +30,7 @@ export default function StripeForm({verCode,userFromUrl,userFromLogin}) {
     },
   };
 
-  var seeMoreFeatures = <a href="/#" onClick={handleSeeMoreFeatures} style={{cursor:"pointer",color:"#000000",textDecoration:"none"}}>See more features</a>;
+  var seeMoreFeatures = <a href="https://www.flora-sol.com/" style={{cursor:"pointer",color:"#000000",textDecoration:"none",letterSpacing:"1"}}>SEE MORE FEATURES</a>;
 
   /*slide1: {
       background: '#FEA900',
@@ -109,7 +109,7 @@ const ProductDisplay = () => (
 				    alignItems="center"
             style={{marginLeft:"50px",marginRight:"10px",backgroundColor:"#ffffff"}}
 			        >
-              <div style={{marginTop:"40px",textAlign:"center",fontWeight:"bold",fontSize:"27px",font:"sans-serif"}}>Basic</div>
+              <div style={{marginTop:"40px",textAlign:"center",fontWeight:"bold",fontSize:"27px"}}>Basic</div>
               <img alt="leafImage1" src={leafImage1} style={{height:leafImageHeight,marginTop:"20px",marginBottom:"15px"}}/>
               <img alt="pricingImage" src={pricingImage1} style={{width:pricingImageWidth,height:pricingImageHeight}}/>
               <div style={{marginTop:"20px",textAlign:"center",fontWeight:"bold",color:"#5b5b5b",fontSize:"18px"}}>Billed monthly</div>
@@ -138,7 +138,7 @@ const ProductDisplay = () => (
               </Grid>
               </Grid>
               <Button variant="contained" aria-controls="simple-menu" aria-haspopup="true" style={{width:"90%",marginRight:"5px",marginLeft:"5px",marginTop:"25px",marginBottom:"10px",backgroundColor:"#047AF6",color:"#FFFFFF"}} onClick={handleGoToBasic}>Start a free trial</Button>
-              <div style={{textAlign:"center",fontWeight:"bold",color:"#5b5b5b",marginBottom:"20px",fontSize:"18px",font:"sans-serif"}}>{seeMoreFeatures}</div>
+              <div style={{textAlign:"center",color:"#5b5b5b",marginBottom:"20px",fontSize:"18px",fontFamily:"Arial, Helvetica, sans-serif"}}>{seeMoreFeatures}</div>
               </Grid>
               <Grid
 				    container
@@ -237,7 +237,7 @@ const ProductDisplayMobile = () => (
               style={{width:formWidth}}
               wrap="nowrap"
 			        >
-              <div style={{marginTop:"20px",textAlign:"center",fontWeight:"bold",color:"#000000",fontSize:"27px",font:"sans-serif"}}>Basic</div>
+              <div style={{marginTop:"20px",textAlign:"center",fontWeight:"bold",color:"#000000",fontSize:"27px"}}>Basic</div>
               <img alt="leafImage1" src={leafImage1} style={{height:leafImageHeight,marginTop:"10px",marginBottom:"10px"}}/>
               <img alt="pricingImage" src={pricingImage1} style={{width:pricingImageWidth,height:pricingImageHeight}}/>
               <Grid
@@ -249,7 +249,7 @@ const ProductDisplayMobile = () => (
             style={{marginTop:"10px"}}
 			        >
               <div style={{marginRight:"30px",fontWeight:"bold",fontSize:"50px"}}>&#8592;</div>
-              <div style={{textAlign:"center",fontWeight:"bold",color:"#5b5b5b",fontSize:"18px"}}>Billed monthly</div>
+              <div style={{textAlign:"center",fontWeight:"bold",color:"#5b5b5b",fontSize:"18px",marginTop:"2px"}}>Billed monthly</div>
               <div style={{marginLeft:"30px",fontWeight:"bold",color:"#000000",fontSize:"50px"}}>&#8594;</div>
               </Grid>
             <Grid
@@ -262,7 +262,7 @@ const ProductDisplayMobile = () => (
               <div style={{marginTop:"2px",textAlign:"center",color:"#000000",fontSize:"23px",fontWeight:"bold"}}>plants per month</div>
               </Grid>
               <Button variant="contained" aria-controls="simple-menu" aria-haspopup="true" style={{width:"90%",marginRight:"5px",marginLeft:"5px",marginTop:"25px",marginBottom:"10px",backgroundColor:"#047AF6",color:"#FFFFFF"}} onClick={handleGoToBasic}>Start a free trial</Button>
-              <div style={{textAlign:"center",fontWeight:"bold",color:"#5b5b5b",marginBottom:"15px",fontSize:"18px", font:"sans-serif"}}>{seeMoreFeatures}</div>
+              <div style={{textAlign:"center",color:"#5b5b5b",marginBottom:"15px",fontSize:"18px", fontFamily:"Arial, Helvetica, sans-serif"}}>{seeMoreFeatures}</div>
               </Grid> 
           </div>
           <div style={Object.assign({}, styles.slide, styles.slide2)}>
