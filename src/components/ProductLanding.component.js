263a5e4a096d095faf3313f064a293cb42de16f3
mitchelltjiii <mitchelltjiii@gmail.com>
2022-05-07 18:03:56 -0700

    com

--- src/components/ProductLanding.component.js
+++ src/components/ProductLanding.component.js
@@ -50,14 +50,14 @@ function ProductLanding({setCurrentPage,logVisit}) {
         alignItems="center"
 style={{backgroundColor:"#444444",height:"100%",width:"100%"}}
       >
-          <div style={{width:"100%",height:"80px",backgroundColor:"#32a852"}}>
+          <div style={{width:"100%",height:"80px",backgroundColor:"#000000"}}>
 <Grid
       container
       direction="column"
         justifyContent="center"
       alignItems="center"
 style={{width:"100%"}}>
-<div style={{width:"70%",marginTop:"10px",marginBottom:"10px",color:"#FFFFFF",backgroundColor:"#e4e4e4"}}>
+<div style={{width:"70%",marginTop:"10px",marginBottom:"10px",color:"#FFFFFF"}}>
       <img alt="logo" src={zhlogotransparent} style={{minHeight:"62px",maxHeight: "62px",float:"left"}}/>
 
 
@@ -69,7 +69,28 @@ style={{width:"100%"}}>
 </Grid>
       
 </div>
-        
+<div style={{backgroundColor:"#444444", position:"absolute",top:"80px",bottom:"0px",left:"0px",right:"0px",display:'flex',alignItems: 'center',justifyContent: 'center'}}>
+          <div style={{width:"70%",marginBottom:"200px"}}>
+                  <div style={{float:"left",backgroundColor:"#444444",width:"60%",height:"95%"}}>
+                  <Grid
+				container
+				direction="column"
+			  	justifyContent="center"
+				alignItems="left">
+          <div style={{fontSize:"55px",fontWeight:"bold",color:"#FFFFFF"}}>WELCOME TO ZIPHARVEST</div>
+          <div style={{fontSize:"22px",color:"#FFFFFF",marginTop:lineSpacing}}>&#10004;Record plant harvest weights faster than ever with a single voice command</div>
+          <div style={{fontSize:"22px",color:"#FFFFFF",marginTop:lineSpacing}}>&#10004;Report harvest batches to Metrc easily with our reliable export feature</div>
+          <div style={{fontSize:"22px",color:"#FFFFFF",marginTop:lineSpacing}}>&#10004;14-day free trial for all users and price tiers available based on usage</div>
+          <div style={{fontSize:"22px",color:"#FFFFFF",marginTop:lineSpacing}}>&#10004;Get started in just a few minutes without any obligation</div>
+          <div style={{fontSize:"22px",color:"#FFFFFF",marginTop:lineSpacing}}>&#10004;No special equipment required, just your voice, phone and the scales you already own</div>
+
+        </Grid>
+                  </div>
+                  <div style={{float:"right",backgroundColor:"#e4e4e4",width:"30%",height:"95%",borderRadius:"5px"}}>
+                  <CreateUserMiniForm></CreateUserMiniForm>
+        </div>
+              </div>
+					</div>
       </Grid>
     );
   }
