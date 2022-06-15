82693a85db5b9b15d257ed7677a68ef11ee46735
mitchelltjiii <mitchelltjiii@gmail.com>
2022-06-15 11:18:34 -0700

    com

--- src/components/StripeForm.js
+++ src/components/StripeForm.js
@@ -98,7 +98,6 @@ const ProductDisplay = () => (
               <div style={{marginTop:"40px",textAlign:"center",fontWeight:"bold",fontSize:"29px"}}>Basic</div>
               <img alt="leafImage1" src={leafImage1} style={{height:leafImageHeight,marginTop:"20px",marginBottom:"15px"}}/>
               <img alt="pricingImage" src={pricingImage1} style={{width:pricingImageWidth,height:pricingImageHeight}}/>
-              <div style={{marginTop:"20px",textAlign:"center",fontWeight:"bold",color:"#5b5b5b",fontSize:"18px"}}>Billed monthly</div>
               <Grid
 				    container
 				    direction="column"
@@ -118,12 +117,13 @@ const ProductDisplay = () => (
             justifyContent="center"
 				    alignItems="center"
 			        >
-              <div style={{marginTop:"25px",textAlign:"center",fontSize:"23px",fontWeight:"bold"}}>Export up to 625</div>
+              <div style={{marginTop:"40px",textAlign:"center",fontSize:"23px",fontWeight:"bold"}}>Export up to 625</div>
               <div style={{marginTop:"2px",textAlign:"center",fontSize:"23px",fontWeight:"bold"}}>plants per month</div>
               </Grid>
               </Grid>
               </Grid>
               <Button variant="contained" aria-controls="simple-menu" aria-haspopup="true" style={{width:"90%",height:"60px",marginRight:"5px",marginLeft:"5px",marginTop:"25px",marginBottom:"10px",backgroundColor:"#444444",color:"#FFFFFF"}} onClick={handleGoToBasic}>Buy Now</Button>
+              <Button variant="contained" aria-controls="simple-menu" aria-haspopup="true" style={{width:"90%",height:"60px",marginRight:"5px",marginLeft:"5px",marginTop:"15px",marginBottom:"10px",backgroundColor:"#444444",color:"#FFFFFF"}} onClick={handleGoToBasicYear}>Get One Year 50% OFF</Button>
               <div style={{textAlign:"center",color:"#5b5b5b",marginBottom:"20px",fontSize:"11px",fontFamily:"Arial, Helvetica, sans-serif",letterSpacing:2}}>{seeMoreFeatures}</div>
               </Grid>
               <Grid
@@ -417,6 +417,10 @@ const handleLeft = () => {
 }
 
 const handleRight = () => {
+}
+
+const handleGoToBasicYear = () => {
+  goToProduct("testyear");
 }
 
 const handleGoToBasic = () => {
