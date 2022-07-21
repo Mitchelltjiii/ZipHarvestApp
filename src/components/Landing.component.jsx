2aa22b7bea3b1a078927a21aa12a28ed63b0c7ac
mitchelltjiii <mitchelltjiii@gmail.com>
2022-07-21 13:28:12 -0700

    com

--- src/components/Landing.component.jsx
+++ src/components/Landing.component.jsx
@@ -21,6 +21,42 @@ getFreeTrialEnds,getReferalLink,getGrantFreeMonthCode,setGrantFreeMonthCode,getF
 setOffer,setNewUsername,getOutdoorOffer}){
 	let fromAccountSettings = true;
 
+	React.useEffect(
+		() =>
+		  navigation.addListener('beforeRemove', (e) => {
+			if (!hasUnsavedChanges) {
+			  // If we don't have unsaved changes, then we don't need to do anything
+			  return;
+			}
+
+			if(!userID.includes("Demo")){
+				console.log("NotD");
+				return;
+			}
+			console.log("Demo");
+	
+			// Prevent default behavior of leaving the screen
+			e.preventDefault();
+	
+			// Prompt the user before leaving the screen
+			Alert.alert(
+			  'Discard changes?',
+			  'You have unsaved changes. Are you sure to discard them and leave the screen?',
+			  [
+				{ text: "Don't leave", style: 'cancel', onPress: () => {} },
+				{
+				  text: 'Discard',
+				  style: 'destructive',
+				  // If the user confirmed, then we dispatch the action we blocked earlier
+				  // This will continue the action that had triggered the removal of the screen
+				  onPress: () => navigation.dispatch(e.data.action),
+				},
+			  ]
+			);
+		  }),
+		[navigation, hasUnsavedChanges]
+	  );
+
     return(
         <div>
 			{currentPage === 'harvest-form' ? (
